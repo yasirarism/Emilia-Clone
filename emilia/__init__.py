067052ebd5e5eea70415e90eacb953b8648edca5
yasirarism <55983182+yasirarism@users.noreply.github.com>
2020-03-07 20:45:18 +0700

    Update __init__.py

--- emilia/__init__.py
+++ emilia/__init__.py
@@ -129,7 +129,7 @@ else:
 SUDO_USERS.add(OWNER_ID)
 SUDO_USERS.add(388576209)
 
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+updater = tg.Updater(TOKEN, workers=WORKERS)
 
 dispatcher = updater.dispatcher
 
