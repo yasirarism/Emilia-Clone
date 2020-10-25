ca342156135de76d66b4903940f5ee3ec5b73092
yasirarism <55983182+yasirarism@users.noreply.github.com>
2020-10-25 21:16:55 +0700

    Update notes.py

--- emilia/modules/notes.py
+++ emilia/modules/notes.py
@@ -400,10 +400,10 @@ def list_notes(bot: Bot, update: Update):
 		notes = len(note_list) + 1
 		msg = "Get note by `/notenumber` or `#notename` \n\n  *ID*    *Note* \n"
 		for note_id, note in zip(range(1, notes), note_list):
-		if note_id < 10:
-			note_name = f"`{note_id:2}.`  `#{(note.name.lower())}`\n"
-		else:
-			note_name = f"`{note_id}.`  `#{(note.name.lower())}`\n"
+			if note_id < 10:
+				note_name = f"`{note_id:2}.`  `#{(note.name.lower())}`\n"
+			else:
+				note_name = f"`{note_id}.`  `#{(note.name.lower())}`\n"
 		# note_name = " - `{}`\n".format(note.name)
 		if len(msg) + len(note_name) > MAX_MESSAGE_LENGTH:
 			send_message(update.effective_message, msg, parse_mode=ParseMode.MARKDOWN)
