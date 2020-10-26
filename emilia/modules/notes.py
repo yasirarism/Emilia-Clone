ed8ffb820d59ee3b4d833a3c6f48726ef07017d3
yasirarism <55983182+yasirarism@users.noreply.github.com>
2020-10-26 12:55:38 +0700

    Update notes.py

--- emilia/modules/notes.py
+++ emilia/modules/notes.py
@@ -376,6 +376,21 @@ def private_note(bot: Bot, update: Update, args: List[str]):
 @run_async
 def list_notes(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    conn = connected(bot, update, chat, user.id, need_admin=False)
+    if conn:
+	chat_id = conn
+	chat_name = dispatcher.bot.getChat(conn).title
+	msg = tl(update.effective_message, "*📝 Catatan di {}:*\n").format(chat_name)
+    else:
+	chat_id = update.effective_chat.id
+	if chat.type == "private":
+		chat_name = ""
+		msg = tl(update.effective_message, "*Catatan lokal:*\n")
+	else:
+		chat_name = chat.title
+		msg = tl(update.effective_message, "*Catatan di {}:*\n").format(chat_name)
     note_list = sql.get_all_chat_notes(chat_id)
     notes = len(note_list) + 1
     msg = "Get note by `/notenumber` or `#notename` \n\n  *ID*    *Note* \n"
