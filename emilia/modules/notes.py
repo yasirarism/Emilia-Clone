fcce3537d480f0488facce0216b7074fa708217d
yasirarism <55983182+yasirarism@users.noreply.github.com>
2020-10-26 12:37:34 +0700

    Update notes.py

--- emilia/modules/notes.py
+++ emilia/modules/notes.py
@@ -417,16 +417,18 @@ def list_notes(bot: Bot, update: Update):
 				chat_name = ""
 				msg = tl(update.effective_message, "<b>Catatan lokal:</b>\n")
 			else:
-				chat_name = chat.title
-				msg = tl(update.effective_message, "<b>Catatan di {}:</b>\n").format(chat_name)
-			for note in note_list:
-				note_name = " - <code>{}</code>\n".format(note.name)
+				note_list = sql.get_all_chat_notes(chat_id)
+				notes = len(note_list) + 1
+				msg = "Get note by `/notenumber` or `#notename` \n\n  *ID*    *Note* \n"
+				for note_id, note in zip(range(1, notes), note_list):
+					if note_id < 10:
+						note_name = f"`{note_id:2}.`  `#{(note.name.lower())}`\n"
+					else:
+						note_name = f"`{note_id}.`  `#{(note.name.lower())}`\n"
 				if len(msg) + len(note_name) > MAX_MESSAGE_LENGTH:
-					send_message(update.effective_message, msg, parse_mode=ParseMode.MARKDOWN)
+					update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
 					msg = ""
 				msg += note_name
-			msg += tl(update.effective_message, "\nAnda dapat mengambil catatan ini dengan menggunakan <code>/get notename</code>, atau <code>#notename</code>")
-			send_message(update.effective_message, msg, parse_mode=ParseMode.HTML)
 
 
 def __import_data__(chat_id, data):
